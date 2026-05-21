140194d09bedcff52ce252bf155026f871dbd98d
syfar-vistajam <Mohammed.Fardeen@vistajam.com>
2026-05-22 00:37:13 +0530

    subarraysDivByK LC 974

---
+++ src/main/java/org/example/Leetcode/PrefixSum/SubarraysDivByK.java
@@ -0,0 +1,62 @@
+package org.example.Leetcode.PrefixSum;
+
+import java.util.HashMap;
+
+public class SubarraysDivByK {
+
+    /*
+    * 974. Subarray Sums Divisible by K
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+
+A subarray is a contiguous part of an array.
+
+
+
+Example 1:
+
+Input: nums = [4,5,0,-2,-3,1], k = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by k = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+Example 2:
+
+Input: nums = [5], k = 9
+Output: 0
+
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-104 <= nums[i] <= 104
+2 <= k <= 104*/
+
+
+    public int subarraysDivByK(int[] nums, int k) {
+        int sum =0 , count =0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int num : nums){
+            sum +=num;
+            int sumbyk = sum%k;
+            if(sumbyk < 0){
+                sumbyk += k;
+            }
+            if(map.containsKey(sumbyk)){
+                count+=map.get(sumbyk);
+            }
+            map.put(sumbyk, map.getOrDefault(sumbyk,0)+1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        SubarraysDivByK subarraysDivByK = new SubarraysDivByK();
+        System.out.println(subarraysDivByK.subarraysDivByK(new int[]{4,5,0,-2,-3,1},5));
+    }
+
+}
